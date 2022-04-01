a1ecbab3111d2be90d47505c347afa82dc67893b
SooHyun3Lee <my_ant1@naver.com>
2022-04-01 10:44:34 -0400

    Unity initial setups

---
+++ SWFNAF/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = FindObjectOfType<GameManager>();
+            }
+
+            return m_instance;
+        }
+    }
+
+    public static GameManager m_instance;
+
+    private void Awake()
+    {
+        if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
