1e4ddd90219714a07e098b4207d7bdb538a68621
npackard <nicolespackard@gmail.com>
2022-04-06 08:53:26 -0400

    player looks around with AD and arrow keys instead of mouse

--- SWFNAF/Assets/Scripts/Night/NightPlayerLook.cs
+++ SWFNAF/Assets/Scripts/Night/NightPlayerLook.cs
@@ -8,29 +8,51 @@ public class NightPlayerLook : MonoBehaviour
     public float mouseSensitivity = 1f;
     public float smoothing = 2f;
 
-    public float minY = -45f;
-    public float maxY = 45f;
+    public float minY = -30f;
+    public float maxY = .0f;
 
     private float change;
     private float smooth;
-
     private float mouse;
+
+    private bool leftTrigger = false;
+    private bool rightTrigger = false;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (leftTrigger) {
+            anim.ResetTrigger("lookLeft");
+            leftTrigger = false;
+        }
+        if (rightTrigger) {
+            anim.ResetTrigger("lookRight");
+            rightTrigger = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+            anim.SetTrigger("lookLeft");
+            leftTrigger = true;
+        } else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            anim.SetTrigger("lookRight");
+            rightTrigger = true;
+        }
+
+        /*
         float sensitivity = mouseSensitivity / 10f;
         change = Input.GetAxisRaw("Mouse X");
         Vector2 mouseDir = Vector2.Scale(new Vector2(change, 0f), new Vector2(sensitivity * smoothing, sensitivity * smoothing));
         smooth = Mathf.Lerp(smooth, mouseDir.x, 1f / smoothing);
         mouse += smooth;
         transform.localRotation = Quaternion.AngleAxis(Mathf.Clamp(mouse, minY, maxY), transform.up);
+        */
     }
 }
 
