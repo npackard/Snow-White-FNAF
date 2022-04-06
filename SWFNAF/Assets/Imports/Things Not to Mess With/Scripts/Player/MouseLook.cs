240f7a794c5ed3da521b223cf3e08a8c9ca5bcde
npackard <nicolespackard@gmail.com>
2022-04-05 23:10:59 -0400

    untextured dwarf house

---
+++ SWFNAF/Assets/Imports/Things Not to Mess With/Scripts/Player/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 200;
+    public Transform playerCamera;
+    public bool cursorLock;
+
+    float xRotation = 20f;
+
+    void Start()
+    {
+        if(cursorLock)
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 40f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
