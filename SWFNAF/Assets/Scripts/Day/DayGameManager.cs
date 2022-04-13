aa974eb85d385d9dc3f757e7f29eb6040d668544
Alan <my_ant1@naver.com>
2022-04-12 21:02:57 -0400

    Day Door interaction added

--- SWFNAF/Assets/Scripts/Day/DayGameManager.cs
+++ SWFNAF/Assets/Scripts/Day/DayGameManager.cs
@@ -19,9 +19,18 @@ public class DayGameManager : MonoBehaviour
 
     public static DayGameManager m_instance;
 
+    private bool[] doorKeys;
+    public void GetKey(int i) { doorKeys[i] = true; }
+    public bool CheckKey(int i) { return doorKeys[i]; }
+
     private bool[] dwarfGems;
     public void GetGem(int i) { if (!dwarfGems[i]) { dwarfGems[i] = true; gemCount++; } }
     public int gemCount = 1;
+
+    private int inGameTime = 0;
+    private float realTime = 0;
+    private float unitTime = 10; // 15 min
+    private float maxTime = 48; // 12 hours
 
     private void Awake()
     {
@@ -34,12 +43,30 @@ public class DayGameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        doorKeys = new bool[5];
+        doorKeys[0] = true;
+
         dwarfGems = new bool[6];
+        inGameTime = 0;
+        DayUIManager.instance.UpdateTime(inGameTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (inGameTime <= maxTime)
+        {
+            realTime += Time.fixedDeltaTime;
+            if (realTime >= unitTime)
+            {
+                realTime = 0;
+                inGameTime += 1;
+                if (inGameTime > maxTime)
+                {
+                    // switch to night time
+                    return;
+                }
+                DayUIManager.instance.UpdateTime(inGameTime);
+            }
+        }
     }
 }
