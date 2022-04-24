fb6ded3744206bd2ac854e339c0d96dad3bba215
npackard <nicolespackard@gmail.com>
2022-04-24 16:11:52 -0400

    improved player look controls

--- SWFNAF/Assets/Scripts/Night/NightPlayerLook.cs
+++ SWFNAF/Assets/Scripts/Night/NightPlayerLook.cs
@@ -4,45 +4,32 @@ using UnityEngine;
 
 public class NightPlayerLook : MonoBehaviour
 {
-    [Range(0, 1)]
-    public float mouseSensitivity = 1f;
-    public float smoothing = 2f;
-
-    public float minY = -30f;
-    public float maxY = .0f;
+    public float minY = 182.5f;
+    public float maxY = 242.5f;
+    public float speed = .5f;
 
     private float change;
     private float smooth;
     private float mouse;
-
-    private bool leftTrigger = false;
-    private bool rightTrigger = false;
-    private Animator anim;
+    private float newY;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (leftTrigger) {
-            anim.ResetTrigger("lookLeft");
-            leftTrigger = false;
+        if (Input.mousePosition.x < Screen.width / 5) {
+            newY -= speed;
+        } else if (Input.mousePosition.x > 4 * Screen.width / 5) {
+            newY += speed;
         }
-        if (rightTrigger) {
-            anim.ResetTrigger("lookRight");
-            rightTrigger = false;
-        }
+        if (newY < 0) newY = 360 + newY;
+        newY = Mathf.Clamp(newY, minY, maxY);
+        transform.rotation = Quaternion.Euler(transform.rotation.x, newY, transform.rotation.z);
 
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-            anim.SetTrigger("lookLeft");
-            leftTrigger = true;
-        } else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
-            anim.SetTrigger("lookRight");
-            rightTrigger = true;
-        }
     }
 }
