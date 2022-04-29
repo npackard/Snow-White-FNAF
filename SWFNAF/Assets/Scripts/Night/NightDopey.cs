cbfbe82496ac8dede6a89fbbc72a6636666d55b1
npackard <nicolespackard@gmail.com>
2022-04-28 23:52:12 -0400

    dopey poof sound

--- SWFNAF/Assets/Scripts/Night/NightDopey.cs
+++ SWFNAF/Assets/Scripts/Night/NightDopey.cs
@@ -5,33 +5,39 @@ using UnityEngine;
 public class NightDopey : MonoBehaviour
 {
     public Animator anim;
+    public AudioClip poof;
+
+    private AudioSource audio;
 
     private bool fire;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        audio = GetComponent<AudioSource>();
     }
 
     public void Home() {
         fire = false;
+        audio.PlayOneShot(poof);
         anim.SetInteger("position", 0);
     }
 
     public void Fire() {
         fire = true;
+        audio.PlayOneShot(poof);
         anim.SetInteger("position", 1);
     }
 
     public void Door() {
         fire = false;
-        anim.SetBool("door", true);
+        audio.PlayOneShot(poof);
         anim.SetInteger("position", 2);
     }
 
     public void Vent() {
         fire = false;
+        audio.PlayOneShot(poof);
         anim.SetInteger("position", 3);
     }
 
